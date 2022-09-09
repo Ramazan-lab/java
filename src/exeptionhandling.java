e097957c021a64d99024ee0f4c98ea0c63f1dc0c
RamazanYilmaz <=>
2022-09-09 16:17:20 +0300

    m

---
+++ src/exeptionhandling.java
@@ -0,0 +1,12 @@
+public class exeptionhandling {
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3,4};
+            System.out.println(myNumbers[3]);
+        } catch (Exception e) {
+            System.out.println("hata bulundu.");
+        } finally {
+            System.out.println("try catch işlemi bitti");
+        }
+    }
+}

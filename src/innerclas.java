51141177bd9b44d9d6bf8725f73c6030b1769d78
RamazanYilmaz <=>
2022-09-12 14:45:14 +0300

    m

---
+++ src/innerclas.java
@@ -0,0 +1,24 @@
+class outer {
+    int x = 10;
+
+    class inner {
+        int y = 5;
+        public int myInnerMethod() {
+            return x;
+        }
+    }
+}
+
+public class innerclas {
+    public static void main(String[] args) {
+        //method static olursa outer sınıfından method oluşturmadan inner sınıfına ulaşılabılir
+        //outer.inner myInner = new outer.inner();
+       // System.out.println(myInner.y);
+
+        //inner methodundan outere ulaşma
+        outer myOuter = new outer();
+        outer.inner myInner =myOuter.new inner();
+        System.out.println(myInner.myInnerMethod());
+    }
+}
+

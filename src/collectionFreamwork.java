5be3f246ae4c0a59e86e4bfcadc25ddb3e2da2e5
RamazanYilmaz <=>
2022-09-12 13:17:33 +0300

    m

--- src/collectionFreamwork.java
+++ src/collectionFreamwork.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.*;
 
-public class accessMod {
+public class collectionFreamwork {
     public static void main(String[] args){
         HashSet<String> hs= new HashSet<String>();
             hs.add("ahmet");
---
+++ src/overloading.java
@@ -0,0 +1,20 @@
+public class overloading {
+    private String formatNumber(int value) {
+        return String.format("%d", value);
+    }
+
+    private String formatNumber(double value) {
+        return String.format("%.3f", value);
+    }
+
+    private String formatNumber(String value) {
+        return String.format("%.2f", Double.parseDouble(value));
+    }
+
+    public static void main(String[] args) {
+        overloading hs = new overloading();
+        System.out.println(hs.formatNumber(500));
+        System.out.println(hs.formatNumber(89.9934));
+        System.out.println(hs.formatNumber("550"));
+    }
+}
